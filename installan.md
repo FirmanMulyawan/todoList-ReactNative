20c7930bfb5b6c3b29f9c904c20ebe57d8380b4a
FirmanMulyawan <firmanmulyawan491@gmail.com>
2020-04-14 15:48:16 +0700

    first commit

---
+++ installan.md
@@ -0,0 +1,14 @@
+<!-- Step 1: Install react-native-elements -->
+
+yarn add react-native-elements
+
+# or with npm
+
+npm i react-native-elements --save
+
+npm i --save react-native-vector-icons
+
+<!-- cache npm  -->
+
+npm cache clean --force
+npm audit fix
---
+++ pararenting/app6.js
@@ -0,0 +1,58 @@
+/* Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native';
+
+export default function App() {
+  const [people, setPeople] = useState([
+    {name: 'shaun', id: '1'},
+    {name: 'yoshi', id: '2'},
+    {name: 'mario', id: '3'},
+    {name: 'luigi', id: '4'},
+    {name: 'peach', id: '5'},
+    {name: 'toad', id: '6'},
+    {name: 'bowser', id: '7'},
+  ]);
+
+  const pressHandler = id => {
+    setPeople(prevPeople => {
+      return prevPeople.filter(person => person.id !== id);
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        numColumns={2}
+        keyExtractor={item => item.id}
+        data={people}
+        renderItem={({item}) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
+    marginHorizontal: 10,
+  },
+});
